44297e228d88e5d1bfd69d0488681e93e44f7e0a
Ed-dawardo <112400883+Ed-dawardo@users.noreply.github.com>
2025-05-31 17:43:50 -0400

    Update state.sh

--- state.sh
+++ state.sh
@@ -1,3 +1,11 @@
+## Question 1: 
+
+## - Create a simple shell script called `state.sh` that will append info listed below into `/tmp/state.log` file:
+##  - current date and time
+##  - current load and users logged in
+##  - all processes running on the machine (long listing that, among other info, includes PID and PPID )
+## - Create a cron job that will execute this script every 5 minutes (the state info gets appended into file /tmp/state.log every five minutes)
+
 #!/bin/bash
 echo "Date: $(date)" >> /tmp/state.log
 echo "Uptime (load and users)" >> /tmp/state.log
